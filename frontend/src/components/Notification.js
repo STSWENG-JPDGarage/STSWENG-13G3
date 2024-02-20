f52202eca885f061543d96937c0666299c4dbd16
ReignLarraquel <reignlarraquel@gmail.com>
2024-02-20 18:01:26 +0800

    Fix Notification design

--- frontend/src/components/Notification.js
+++ frontend/src/components/Notification.js
@@ -1,26 +1,25 @@
 import Toast from 'react-bootstrap/Toast';
-import Stack from 'react-bootstrap/Stack';
+import {Row, Col} from 'react-bootstrap'
+import CloseButton from 'react-bootstrap/CloseButton';
 
-function Notification() {
+
+const Notification = () => {
   return (
     <Toast>
-      <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-      
-      <Stack direction="horizontal" gap={2}>
-      <strong className="me-auto">Alpinestars SMX-1 Drystar Gloves</strong>
-      <img></img> //X icon here
-      </Stack>
-      
-      <Stack direction="horizontal" gap={2}>
-      <small>A customer has just placed an order for thi...</small>
-      </Stack>
-
-      <Stack direction="horizontal" gap={2}>
-      <small>11 mins ago</small>
-      <img src></img>
-      </Stack>
+      <Toast.Body>
+        <Row>
+          <Col><p className='txt-16 fw-bold'>Alpinestars SMX-1 Drystar Gloves</p></Col>
+          <Col class='w-auto'><CloseButton /></Col>
+          
+        </Row>
+        <Row><p className='txt-14'>A customer has just placed an order for thi...</p></Row>
+        <Row>
+          <Col><p>2 hours ago</p></Col>
+          <Col class='w-auto'><img src="icon_cart_.png"/></Col>
+        </Row>
+      </Toast.Body>
     </Toast>
   );
-}
+};
 
 export default Notification;
