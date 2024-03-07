a8a407ece6e62efeb29486b3f643ed1a4e7ced7e
Harold <harold.mojica03@gmail.com>
2024-03-08 05:20:48 +0800

    Fix Routes of Payment Reminder

--- backend/routes/paymentReminder.js
+++ backend/routes/paymentReminder.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const paymentReminderController = require('../controllers/paymentReminderController');
 
-router.post('/paymentReminders', paymentReminderController.createPaymentReminder);
-router.get('/paymentReminders', paymentReminderController.getAllPaymentReminders);
-router.get('/paymentReminders/:id', paymentReminderController.getPaymentReminderById);
-router.put('/paymentReminders/:id', paymentReminderController.updatePaymentReminderById);
-router.delete('/paymentReminders/:id', paymentReminderController.deletePaymentReminderById);
+router.post('/paymentReminders-create', paymentReminderController.createPaymentReminder);
+router.get('/paymentReminders-get', paymentReminderController.getAllPaymentReminders);
+router.get('/paymentReminders-get/:id', paymentReminderController.getPaymentReminderById);
+router.put('/paymentReminders-update/:id', paymentReminderController.updatePaymentReminderById);
+router.delete('/paymentReminders-delete/:id', paymentReminderController.deletePaymentReminderById);
 
 module.exports = router;
