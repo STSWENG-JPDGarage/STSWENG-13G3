628a7ec85b9b05e0bc05ab65df77fda94a7c0d41
Harold <harold.mojica03@gmail.com>
2024-03-08 03:56:17 +0800

    Add Non-Archive and Archive Notification Count

--- frontend/src/components/NotificationPanel.js
+++ frontend/src/components/NotificationPanel.js
@@ -1,10 +1,36 @@
-import {Container} from 'react-bootstrap'
+import {Container, Badge} from 'react-bootstrap'
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Notification from '../components/Notification';
-
+import { DOMAIN } from '../config'
+import React, { useState, useEffect } from 'react';
 
 const NotificationPanel = () => {
+  // BACKEND for fetching notifications
+  const [nonArchiveNotificationCount, setNonArchiveNotificationCount] = useState(0);
+  const [archiveNotificationCount, setArchiveNotificationCount] = useState(0);
+
+  useEffect(() => {
+    fetchNotificationsCount();
+  }, []);
+
+  const fetchNotificationsCount = async () => {
+    try {
+      const response = await fetch(`${DOMAIN}/notification/notifications-get`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch notifications');
+      }
+      const notifications = await response.json();
+      const nonArchiveNotifications = notifications.filter(notification => notification.isArchive === "No");
+      const archiveNotifications = notifications.filter(notification => notification.isArchive === "Yes");
+      setNonArchiveNotificationCount(nonArchiveNotifications.length);
+      setArchiveNotificationCount(archiveNotifications.length);
+    } catch (error) {
+      console.error('Error fetching notifications count:', error);
+    }
+  };
+
+  // FRONTEND for notification panel
   return (
     <Container className='bg-white py-4'>
       <Tabs
@@ -12,7 +38,7 @@ const NotificationPanel = () => {
       id="uncontrolled-tab-example"
       className="mb-3 my-0 h-100"
       justify>
-      <Tab eventKey="updates" title="Updates">
+      <Tab eventKey="updates" title={<span>Updates <Badge>{nonArchiveNotificationCount}</Badge></span>}>
       <Notification/>
       <Notification/>
       <Notification/>
@@ -20,7 +46,7 @@ const NotificationPanel = () => {
       <Notification/>
       <Notification/>
       </Tab>
-      <Tab eventKey="archive" title="Archive">
+      <Tab eventKey="archive" title={<span>Archive <Badge>{archiveNotificationCount}</Badge></span>}>
         <Notification/>
       </Tab>
     </Tabs>
