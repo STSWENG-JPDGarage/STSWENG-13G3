4476878f9803a8eb4c88bcbcd7284ae4ec9cfabd
Harold <harold.mojica03@gmail.com>
2024-03-09 21:41:05 +0800

    Update Notification Message

--- frontend/src/components/Notification.js
+++ frontend/src/components/Notification.js
@@ -16,10 +16,10 @@ const Notification = ({ message, stockRemaining, timeElapsed, isArchive, onClose
     <Toast className='bg_card2-red'>
       <Toast.Body>
         <Row>
-          <Col sm={10}><p className='txt-16 fw-bold'>{message}</p></Col>
+          <Col sm={10}><p className='txt-16 fw-bold'>ITEM STOCK ALERT</p></Col>
           <Col sm={2} className='d-flex justify-content-end'><CloseButton onClick={onClose}/></Col>
         </Row>
-        <Row><p className='txt-14'>Status: {stockStatus}<br />Stock remaining: {stockRemaining}</p></Row>
+        <Row><p className='txt-14'>Item <b>{message}</b> is currently at <b>{stockStatus}</b> status with <b>{stockRemaining}</b> stocks remaining.</p></Row>
         <Row>
           <Col><p>{timeElapsed}</p></Col>
           <Col className='d-flex justify-content-end pe-3'><img src="icon_cart_.png" className="w-25"/></Col>
