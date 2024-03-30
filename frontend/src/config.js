ec0c03397341a7f4e65135b641b2e0de3eb55149
enzzzoooo <lorenzo_ambrosio@dlsu.edu.ph>
2024-03-30 16:25:54 +0800

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,7 +3,6 @@ let DOMAIN;
 const temp = "preview"
 
 if (temp === 'production') {
-Payment-System-Design
     DOMAIN = "https://stsweng-13-backend.vercel.app";  // Production domain
 } else if (temp === 'preview') {
     DOMAIN = "https://stsweng-13-backend.vercel.app";  // Development domain
