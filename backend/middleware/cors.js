85db4ddfe298ae967e3bc4b98362eee22ddeca5f
enzzzoooo <lorenzo_ambrosio@dlsu.edu.ph>
2024-04-01 09:11:48 +0800

    fix CD Issues

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -1,6 +1,6 @@
 const corsMiddleware = (req, res, next) => {
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', 'https://stsweng-13-g3-frontend.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
